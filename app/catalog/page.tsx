eeb8410e116d635c72cf2fa5949dfa653356a4d3
Andriy Sak <andreyysak17@gmail.com>
2025-10-01 19:30:44 +0300

    add switch for choice display view of products

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import { siteConfig } from "@/config/site";
 import { Select, SelectItem } from "@heroui/select";
 import { NumberInput } from "@heroui/number-input";
+import { Switch } from "@heroui/switch"
+import { cn } from "@heroui/theme";
 
 export default function CatalogPage() {
   const [selectedCategory, setSelectedCategory] = useState<string | null>(null)
@@ -14,7 +16,7 @@ export default function CatalogPage() {
   const handleChangePerPage = (value: number) => setPerPage(value);
 
   return (
-    <div className="flex justify-between w-full flex-wrap md:flex-nowrap gap-4">
+    <div className="flex justify-between items-center w-full flex-wrap md:flex-nowrap gap-4">
       <Select
         color="primary"
         className="max-w-xs"
@@ -40,6 +42,28 @@ export default function CatalogPage() {
         label="Products per page"
         placeholder="Enter the amount"
       />
+
+      <Switch
+        classNames={{
+          base: cn(
+            "inline-flex flex-row-reverse w-full max-w-md bg-content1 hover:bg-content2 items-center",
+            "justify-between cursor-pointer rounded-lg gap-2 p-4 border-2 border-transparent",
+            "data-[selected=true]:border-primary",
+          ),
+          wrapper: "p-0 h-4 overflow-visible",
+          thumb: cn(
+            "w-6 h-6 border-2 shadow-lg",
+            "group-data-[hover=true]:border-primary",
+            "group-data-[selected=true]:ms-6",
+            "group-data-[pressed=true]:w-7",
+            "group-data-pressed:group-data-selected:ms-4",
+          ),
+        }}
+      >
+        <div className="flex flex-col gap-1">
+          <p className="text-medium">Toggle product view</p>
+        </div>
+      </Switch>
     </div>
   );
 }
